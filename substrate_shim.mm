579c1acdd1517d5f1a3358efcdb6a2e1c62f5669
LO <lo@local>
2026-09-13 21:45:25 +0800

    fix: extern C on MSHookMessageEx shim

--- substrate_shim.mm
+++ substrate_shim.mm
@@ -1,18 +1,24 @@
 /* Substrate 兼容垫片：用 ObjC Runtime 实现 MSHookMessageEx
- * 使 Logos 生成的 hook 代码在 macOS 上无需 CydiaSubstrate 即可运行 */
+ * 使 Logos 生成的 hook 代码在 macOS/iOS 模拟器上无需 CydiaSubstrate 即可运行
+ * 注意：必须 extern "C"，与 shim-include/substrate.h 的声明保持一致 */
 #import <objc/runtime.h>
 #import <objc/message.h>
 #import <Foundation/Foundation.h>
 
-typedef IMP (*MSHookMessageExFunc)(Class, SEL, IMP, IMP *);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void MSHookMessageEx(Class cls, SEL sel, IMP newImp, IMP *origOut) {
     Method m = class_getInstanceMethod(cls, sel);
     if (!m) {
-        // 类方法兜底
         m = class_getClassMethod(cls, sel);
     }
     if (!m) return;
     if (origOut) *origOut = method_getImplementation(m);
     method_setImplementation(m, newImp);
 }
+
+#ifdef __cplusplus
+}
+#endif
